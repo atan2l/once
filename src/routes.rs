289d2c6235144480f3325e2f907dfdd26e0ad2ef
Carlos Amores <cadenaskarlos@gmail.com>
2025-08-03 19:11:08 +0200

    revert: Session state with valkey

--- src/routes.rs
+++ src/routes.rs
@@ -1,8 +1,8 @@
+use crate::app_state::AppState;
 use crate::routes::test::get_test;
 use crate::routes::token::get_token;
 use axum::Router;
 use axum::routing::get;
-use crate::app_state::AppState;
 
 mod test;
 mod token;
